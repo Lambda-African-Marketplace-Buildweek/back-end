a3893fd2700b8dd7a68c8ccd02caa534628b21b3
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-20 18:08:56 -0400

    Fixed typos

--- data/migrations/20201020113354_init.js
+++ data/migrations/20201020113354_init.js
@@ -1,24 +1,12 @@
 
 exports.up = function(knex) {
-  return knex.schema
-    // .createTable('locations', tbl => {
-    //   tbl.increments();
-
-    //   tbl.string('location', 128);
-    // })
-    
+  return knex.schema    
     .createTable('owners', tbl => {
       tbl.increments(); 
 
-      tbl.text('name', 128).unique().notNullable();
-      tbl.text('password', 128).notNullable();
+      tbl.text('username', 128).unique().notNullable();
+      tbl.text('password', 128).unique().notNullable();
 
-    //   tbl
-    //     .integer('item')
-    //     .unsigned()
-    //     .references('items.id')
-    //     .onDelete('RESTRICT')
-    //     .onUpdate('CASCADE');
     })
     .createTable('items', tbl => {
       tbl.increments();
@@ -27,13 +15,7 @@ exports.up = function(knex) {
       tbl.string('description', 512).unique().notNullable();
       tbl.decimal('price', 10, 2).notNullable();
       tbl.string('location', 128).notNullable();
-      // tbl
-      //   .integer('location')
-      //   .unsigned()
-      //   .references('locations.id')
-      //   .onDelete('RESTRICT')
-      //   .onUpdate('CASCADE')
-
+  
       tbl
         .integer('owner')
         .unsigned()
